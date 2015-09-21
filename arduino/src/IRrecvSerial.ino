e4d19c9b227f5aa284cd1adfb95d39fd182911f8
Yuichi Tateno <hotchpotch@gmail.com>
2015-09-21 23:36:26 +0900

    init commit

---
+++ arduino/src/IRrecvSerial.ino
@@ -0,0 +1,35 @@
+#include <IRremote.h>
+
+int RECV_PIN = 11;
+int WAIT = 50;
+
+IRrecv irrecv(RECV_PIN);
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn();
+  Serial.println("SETUP");
+}
+
+decode_results results;
+unsigned long last = 0;
+
+void loop()
+{
+  if (irrecv.decode(&results)) {
+    if (last + WAIT < WAIT) {
+      // for millis() overflow, uptime over 50 days.
+      Serial.println("overflow");
+      last = 0;
+    }
+
+    if (millis() - last > WAIT) {
+      Serial.print("IR,");
+      Serial.print(results.value, HEX);
+      Serial.println("");
+    }
+    last = millis();
+    irrecv.resume();
+  }
+}
